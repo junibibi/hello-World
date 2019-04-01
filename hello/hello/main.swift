4afc685598b2d3906859a39e378d46f234e4158d
박준희 <junibibi04@gmail.com>
2019-04-01 23:58:46 +0900

    연습1

---
+++ hello/hello/main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  hello
+//
+//  Created by JH on 01/04/2019.
+//  Copyright © 2019 JH. All rights reserved.
+//
+
+import Foundation
+
+var str = "Hello, iOs World"
+
+print(str)
+
+
+
